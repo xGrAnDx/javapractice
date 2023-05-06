cb5a3d726e5d28b69876fd1daf8b74df48f3f648
xGrAnDx <grand61rus@gmail.com>
2023-05-06 19:06:52 +0300

    Lesson 3 HW5 pt2

--- src/main/java/ru/grand/hw5/StringMorpher.java
+++ src/main/java/ru/grand/hw5/StringMorpher.java
@@ -1,2 +1,32 @@
-package ru.grand.hw5;public class StringMorpher {
+package ru.grand.hw5;
+
+public class StringMorpher {
+    private int maskLength;
+
+    public StringMorpher(int strL)
+    {
+        if (strL <= 0)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        this.maskLength = strL;
+    }
+
+    public String applyMask(int applyTo)
+    {
+        if (applyTo < 0)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        var numStr = Integer.toString(applyTo);
+
+        if (numStr.length() >= this.maskLength)
+        {
+            return numStr;
+        }
+
+        return String.format("%0" + maskLength + "d", applyTo);
+    }
 }
--- src/main/java/ru/grand/hw5/hw5.java
+++ src/main/java/ru/grand/hw5/hw5.java
@@ -1,2 +1,34 @@
-package ru.grand.hw5;public class hw5 {
+package ru.grand.hw5;
+
+public class hw5 {
+    public static void main(String[] args)
+    {
+        /*task 1*/
+        var tmp1 = new jdkClass(2);
+        var tmp2 = new jdkClass(2);
+        var tmp3 = new jdkClass(3);
+
+        var lmb1 = new lombokClass(3);
+        var lmb2 = new lombokClass(3);
+        var lmb3 = new lombokClass(4);
+
+        System.out.println(String.format("tmp1 = tmp2 ?: [%s]", tmp1.equals(tmp2)));
+        System.out.println(String.format("tmp1 = tmp3 ?: [%s]", tmp1.equals(tmp3)));
+
+        System.out.println(String.format("lmb1 = lmb2 ?: [%s]", lmb1.equals(lmb2)));
+        System.out.println(String.format("lmb1 = lmb3 ?: [%s]", lmb1.equals(lmb3)));
+
+        /*task 2*/
+        var child = new Child();
+
+        /*task 3*/
+
+        var morpher = new StringMorpher(6);
+        System.out.println(morpher.applyMask(0));
+        System.out.println(morpher.applyMask(1));
+        System.out.println(morpher.applyMask(12));
+        System.out.println(morpher.applyMask(123));
+        System.out.println(morpher.applyMask(1234));
+        System.out.println(morpher.applyMask(1234567));
+    }
 }
