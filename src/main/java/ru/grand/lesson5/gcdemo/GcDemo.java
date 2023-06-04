6684336c79579cf24f85f64282998c29041f5e7b
xGrAnDx <grand61rus@gmail.com>
2023-06-04 11:29:59 +0300

    Missing tasks

--- src/main/java/ru/grand/lesson5/gcdemo/GcDemo.java
+++ src/main/java/ru/grand/lesson5/gcdemo/GcDemo.java
@@ -4,6 +4,10 @@ import ru.grand.lesson5.dumpdemo.MemHeavy;
 
 public class GcDemo {
     public static void main(String[] args) {
-
+        while(true)
+        {
+            var heavyMember = new MemHeavy();
+            heavyMember = null;
+        }
     }
 }
---
+++ src/main/java/ru/grand/lesson5/gcdemo/MemHeavy.java
@@ -0,0 +1,22 @@
+package ru.grand.lesson5.gcdemo;
+
+import java.util.ArrayList;
+
+public class MemHeavy {
+    private ArrayList<String> KindaBig = new ArrayList<>();
+
+    private ArrayList<Integer> KindaBigToo = new ArrayList<>();
+
+    public MemHeavy()
+    {
+        for(int i=0;i<300;i++)
+        {
+            var str = new String("sample");
+            this.KindaBig.add(str);
+
+            var refInt = 10;
+
+            this.KindaBigToo.add(refInt);
+        }
+    }
+}
